324669ea1bf428a74fc787b97333e8d506e93f08
KwhoaKai <31353758+KwhoaKai@users.noreply.github.com>
2019-11-14 00:35:47 -0500

    - Style fixes

--- src/edu/cs3500/spreadsheets/model/WorkSheetModel.java
+++ src/edu/cs3500/spreadsheets/model/WorkSheetModel.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.HashMap;
 import java.util.Stack;
 
 
--- src/edu/cs3500/spreadsheets/sexp/SList.java
+++ src/edu/cs3500/spreadsheets/sexp/SList.java
@@ -4,7 +4,6 @@ package edu.cs3500.spreadsheets.sexp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
